3cf04bce5a2670d5cc85c4477836fabc3202ce66
Erzhan <95659303+rzhanito@users.noreply.github.com>
2023-05-25 20:59:09 +0600

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        for(int i = 'A'; i <= 'Z'; i++){
+            System.out.println(i);
+        }
     }
 }
