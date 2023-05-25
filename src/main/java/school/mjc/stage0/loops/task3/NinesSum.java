e79f3df77898de07aaa71c8248090afa7fbe8891
Erzhan <95659303+rzhanito@users.noreply.github.com>
2023-05-26 00:12:36 +0600

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-        int sum = 0;
+        long sum = 0;
         for(int i = 9; i <= lengthOfLastNumber; i = i * 10 + 9){
             sum += i;
         }
